a90405b26acec610e2ded6e580256f26ad84a49c
Jeremy Green <jeremy-green@users.noreply.github.com>
2025-05-18 14:09:26 -0400

    Update embedder __init__.py to include Bedrock embedder

--- graphiti_core/embedder/__init__.py
+++ graphiti_core/embedder/__init__.py
@@ -1,8 +1,11 @@
 from .client import EmbedderClient
 from .openai import OpenAIEmbedder, OpenAIEmbedderConfig
+from .bedrock import BedrockEmbedder, BedrockEmbedderConfig
 
 __all__ = [
     'EmbedderClient',
     'OpenAIEmbedder',
     'OpenAIEmbedderConfig',
+    'BedrockEmbedder',
+    'BedrockEmbedderConfig',
 ]
